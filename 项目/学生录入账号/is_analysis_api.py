978145e078a9cca804f8ccb6b26ea17823301e2d
赵卫东 <313089057@qq.com>
2018-03-28 23:50:46 +0800

    version1

--- 项目/学生录入账号/is_analysis_api.py
+++ 项目/学生录入账号/is_analysis_api.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 #flask_bootstrap:
 #http://www.jb51.net/article/110841.htm
 
 from flask import Flask, request, redirect
-
+from gevent.wsgi import WSGIServer
 from flask import render_template
 from flask_bootstrap import Bootstrap
 import cx_Oracle
@@ -14,12 +14,12 @@ app = Flask(__name__)
 #对bootstrap进行初始化
 bootstrap=Bootstrap(app)
 
-#主页
 # @app.route("/get_github_user",methods=['GET'])
 @app.route("/",methods=['GET'])
 def get_github_user():
-    conn = cx_Oracle.connect('study','123', '202.115.82.8:1521/pdborcl')
+    conn = cx_Oracle.connect('study','Zwd525414', '202.115.82.8:1521/pdborcl')
     cur = conn.cursor()
+    # where id=:p1",{"p1":1})
     sql = "SELECT id,name,class,github_username,update_date FROM is_analysis_studyinfo"
     cur.execute(sql)
     students = cur.fetchall()
@@ -28,7 +28,7 @@ def get_github_user():
 
 # 普通函数,返回学号对应的学生姓名和github账号
 def get_one_user(stu_id):
-    conn = cx_Oracle.connect('study','123', '202.115.82.8:1521/pdborcl')
+    conn = cx_Oracle.connect('study','Zwd525414', '202.115.82.8:1521/pdborcl')
     cur = conn.cursor()
     sql = "SELECT name,github_username FROM is_analysis_studyinfo where id=:id"
     res = cur.execute(sql,{"id":stu_id})
@@ -44,15 +44,15 @@ def get_one_user(stu_id):
 def update_github_user(stu_id):
     if request.method == "POST": #如果是POST方式,表示在修改页面按下了"修改"按钮.
         username = request.form.get('GitHubUserName')
-        conn = cx_Oracle.connect('study', '123', '202.115.82.8:1521/pdborcl')
+        conn = cx_Oracle.connect('study', 'Zwd525414', '202.115.82.8:1521/pdborcl')
         cur = conn.cursor()
         username=username.strip()
         if username != '':
             cur.execute("UPDATE is_analysis_studyinfo SET github_username=:username,update_date=sysdate WHERE id=:id",
-                    {"username":username, "id":stu_id})
+                        {"username":username, "id":stu_id})
         else: #如果没有用户名,要清空日期
             cur.execute("UPDATE is_analysis_studyinfo SET github_username=NULL ,update_date=NULL WHERE id=:id",
-                    {"id":stu_id})
+                        {"id":stu_id})
         conn.commit() # 提交是必须的
         conn.close()
         return redirect('/') #返回主页
@@ -62,4 +62,5 @@ def update_github_user(stu_id):
 
 if __name__ == "__main__":
     #使用https方式将使抓出的IP包无法直接观看.
-    app.run(host="0.0.0.0", port=1522, debug=True) # ,ssl_context='adhoc')
+    #app.run(host="0.0.0.0", port=1522, debug=False) # ,ssl_context='adhoc')
+    WSGIServer(('0.0.0.0',1522),app).serve_forever() #异步非堵塞实现
